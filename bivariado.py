045a2501f877cd23dfcad2ea4ed005e58a052cc6
hlopezo <55169057+hlopezo@users.noreply.github.com>
2023-05-20 19:52:58 -0500

    Update bivariado.py

--- bivariado.py
+++ bivariado.py
@@ -27,5 +27,5 @@ def calcular_estadisticas(tabla, grupo, objetivo):
         'IV': iv * 100,
         'Sum_IV': total_iv * 100
     })
-    result['woe'] = result['woe'].replace([np.inf], 0)
+    result['WOE'] = result['WOE'].replace([np.inf], 0)
     return result
